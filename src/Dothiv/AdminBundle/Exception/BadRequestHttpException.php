ec80ab88ef215db1d685e41e146a8a51ca935118
Markus Tacker <m@coderbyheart.de>
2015-02-04 17:48:14 +0100

    feat(4life): implement changing if name and text
    See https://trello.com/c/Be4HLbKG

--- src/Dothiv/AdminBundle/Exception/BadRequestHttpException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Dothiv\AdminBundle\Exception;
-
-class BadRequestHttpException extends \Symfony\Component\HttpKernel\Exception\BadRequestHttpException implements ExceptionInterface
-{
-}
--- src/Dothiv/AdminBundle/Exception/NotFoundHttpException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Dothiv\AdminBundle\Exception;
-
-class NotFoundHttpException extends \Symfony\Component\HttpKernel\Exception\NotFoundHttpException implements ExceptionInterface
-{
-}
